b71f35679b1d35297a9b0088ac2744a6549329c0
zhanghui <iron.zhang@foxmail.com>
2019-07-15 10:38:03 +0800

    update

--- c32/code.go
+++ c32/code.go
@@ -34,11 +34,91 @@ func isValidParentheses(s string) bool {
 }
 
 func longestValidParentheses(s string) int {
-	return longestValidParenthesesExhaustion(s)
+	return longestValidParenthesesStack(s)
 }
 
 func longestValidParenthesesDP(s string) int {
-	return 0
+	max := 0
+	dp := make([]int, len(s))
+	for i := 1; i < len(dp); i++ {
+		if s[i] == ')' {
+			switch s[i-1] {
+			case '(':
+				if i-2 >= 0 {
+					dp[i] = dp[i-2] + 2
+				} else {
+					dp[i] = 2
+				}
+			case ')':
+				if x := i - dp[i-1] - 1; x >= 0 && s[x] == '(' {
+					if y := x - 1; y >= 0 {
+						dp[i] = dp[y] + dp[i-1] + 2
+					} else {
+						dp[i] = dp[i-1] + 2
+					}
+				}
+			}
+			if dp[i] > max {
+				max = dp[i]
+			}
+		}
+	}
+	return max
+}
+
+func longestValidParenthesesStack(s string) int {
+	max := 0
+	st := make([]int, 0, len(s)+1)
+	st = append(st, -1)
+	for i, c := range s {
+		if c == '(' {
+			st = append(st, i)
+		} else {
+			st = st[:len(st)-1]
+			if len(st) <= 0 {
+				st = append(st, i)
+			} else {
+				if x := i - st[len(st)-1]; x > max {
+					max = x
+				}
+			}
+		}
+	}
+	return max
+}
+
+func longestValidParenthesesLRS(s string) int {
+	var left, right, max int
+	for i := 0; i < len(s); i++ {
+		if s[i] == '(' {
+			left++
+		} else {
+			right++
+		}
+		if left == right {
+			if n := right * 2; n > max {
+				max = n
+			}
+		} else if right > left {
+			left, right = 0, 0
+		}
+	}
+	left, right = 0, 0
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == '(' {
+			left++
+		} else {
+			right++
+		}
+		if left == right {
+			if n := right * 2; n > max {
+				max = n
+			}
+		} else if left > right {
+			left, right = 0, 0
+		}
+	}
+	return max
 }
 
 func longestValidParenthesesExhaustion(s string) int {
